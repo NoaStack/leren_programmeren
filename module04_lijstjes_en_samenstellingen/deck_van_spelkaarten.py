28a7df126962f36a0b326de3652f1e686054fb49
NoaStack <91467633+NoaStack@users.noreply.github.com>
2022-11-27 17:58:01 +0100

    Update deck_van_spelkaarten.py

--- module04_lijstjes_en_samenstellingen/deck_van_spelkaarten.py
+++ module04_lijstjes_en_samenstellingen/deck_van_spelkaarten.py
@@ -2,8 +2,7 @@ import random
 
 
 kleuren = ["harten", "klaveren", "schoppen", "ruiten"]
-kaart_nummer = ["2", "3", "4", "5", "6", "7", "8", "9", "10"]
-kaartenType = ['boer', 'vrouw', 'heer', 'aas']
+kaart_nummer = ["2", "3", "4", "5", "6", "7", "8", "9", "10","boer","vrouw","heer","aas"]
 kaarten = []
 
 
@@ -11,17 +10,14 @@ while len(kaarten) != 52:
     y = random.choice(kleuren)+ ' ' + (random.choice(kaart_nummer))
     if y not in kaarten:
         kaarten.append(y)
-    z = random.choice(kleuren)+ ' ' + (random.choice(kaartenType))
-    if z not in kaarten:
-        kaarten.append(z)
 
 kaarten.append('joker2'), kaarten.append('joker1')
 
-a = 1
-for _ in range(7):
+
+for _ in range(1, 8):
     random_kaart = random.choice(kaarten)
-    print(f"kaart {a}: {random_kaart}")
+    print(f"kaart {_}: {random_kaart}")
     kaarten.remove(random_kaart)
-    a += 1
+    
 print ()
 print(f"deck: {len(kaarten)} {kaarten}")
