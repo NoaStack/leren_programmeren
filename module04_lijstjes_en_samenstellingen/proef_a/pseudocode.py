3b8e245d9f3cc959d1863445002695821419a7f8
NoaStack <99070686@mydavinci.nl>
2022-12-05 15:23:09 +0100

    commit

---
+++ module04_lijstjes_en_samenstellingen/proef_a/pseudocode.py
@@ -0,0 +1,6 @@
+# Deze code vraagt aan de gebruiken een naam daarna voegt die toe bij twee lijsten(deelnemers) en (lootjes).
+# als volgt doe die dat zo lang je namen invult
+# #dan als je geen naam invult en meer dan 3 namen hebt.
+# daarna heb je een for loop die shuffelt zolang de elementen/index van beide lijsten 
+# anders zijn daarna print je een voor een de index 0 tot de lengte van de namen van de lijst
+ 
