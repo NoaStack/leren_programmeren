1bea72824e5650c7e12ba5405a69acf2ffb6b912
NoaStack <99070686@mydavinci.nl>
2022-12-12 13:03:56 +0100

    committed

--- module05_functions/abcd.py
+++ module05_functions/abcd.py
@@ -1,5 +1,17 @@
-a = ["a", "b", "c", "d"]
-import random
+# Python program to display the Fibonacci sequence
 
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
 
-print(random.choice(a))
+nterms = 10
+
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(nterms):
+       print(recur_fibo(i))
---
+++ module05_functions/fibonacci.py
@@ -0,0 +1,10 @@
+def fibonacci(nmr):
+    if nmr <= 1:
+        return nmr
+    else:
+        return(fibonacci(nmr-1)) + fibonacci((nmr-2))
+
+teller = 10
+
+for i in range(teller):
+    print(fibonacci(i))
