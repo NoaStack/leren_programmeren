b6f936745ba0c5fb84935b51196a18e20cefcc98
NoaStack <99070686@mydavinci.nl>
2022-12-16 09:43:42 +0100

    commit

--- module05_functions/berekening_uitvoeren2.py
+++ module05_functions/berekening_uitvoeren2.py
@@ -10,7 +10,7 @@ def multiplication(number1, number2):
 def division(number1, number2):
     return number1 / number2
 
-eerste_ronde = " "
+eerste_ronde = True
 n1 = False
 n2 = False
 keuze = False
@@ -30,7 +30,7 @@ I) niets""")
     keuze = input("""Kies:
 """)
     if keuze == "i":
-        quit()
+        break
     elif keuze == "a":
         som = addition
         reken = "+"
@@ -75,7 +75,7 @@ I) niets""")
                 print("typ een nummer in")            
     elif keuze == "d":
         som = division
-        reken = "x"
+        reken = ":"
         print("Getallen delen")
         while n1 == False:
             try:
@@ -92,21 +92,28 @@ I) niets""")
         reken = "+"
         n2 = 1
         print("Getallen ophogen")
-        while n1 == False:
-            try:
-                n1 = float(input("Welk getal? "))
-            except:
-                print("Typ een nummer in")
     elif keuze == "f":
         som = subtraction
         reken = "-"
         n2 = 1
-        print("Getallen verminderen")
+        print("Getallen verlagen")
+    elif keuze == "g":
+        som = multiplication
+        reken = "x"
+        n2 = 1
+        print("Getallen verdubbelen")
+    elif keuze == "h":
+        som = division
+        reken = ":"
+        n2 = 2
+        print("Getallen halveren")
+    else:
+        continue
     print(f"{n1} {reken} {n2} = {som(number1=n1, number2=n2)}")
     print("--------------------------------------------------")
     print(f"Wat wilt u doen met de uitkomst ({som(number1=n1, number2=n2)})")
     n1 = som(number1=n1, number2=n2)
-    keuze == True
+    keuze = True
     n2 = False
     break
 
@@ -138,22 +145,72 @@ I) niets""")
             try:
                 n2 = float(input(f"Welk getal optellen bij {n1}? "))
             except:
-                print("typ een nummer in") 
+                print("typ een nummer in")
+    elif keuze == "b":
+        som = subtraction
+        reken = "-"
+        print("Getallen aftellen")
+        while n1 == False:
+            try:
+                n1 = float(input("Welk getal? "))
+            except:
+                print("Typ een nummer in")
+        while n2 == False:
+            try:
+                n2 = float(input(f"Welk getal aftrekken bij {n1}? "))
+            except:
+                print("typ een nummer in")
+    elif keuze == "c":
+        som = multiplication
+        reken = "x"
+        print("Getallen vermenigvuldigen")
+        while n1 == False:
+            try:
+                n1 = float(input("Welk getal? "))
+            except:
+                print("Typ een nummer in")
+        while n2 == False:
+            try:
+                n2 = float(input(f"Welk getal vermenigculdigen bij {n1}? "))
+            except:
+                print("typ een nummer in")            
+    elif keuze == "d":
+        som = division
+        reken = ":"
+        print("Getallen delen")
+        while n1 == False:
+            try:
+                n1 = float(input("Welk getal? "))
+            except:
+                print("Typ een nummer in")
+        while n2 == False:
+            try:
+                n2 = float(input(f"Welk getal delen bij {n1}? "))
+            except:
+                print("typ een nummer in")
+    elif keuze == "e":
+        som = addition
+        reken = "+"
+        n2 = 1
+        print("Getallen ophogen")
+    elif keuze == "f":
+        som = subtraction
+        reken = "-"
+        n2 = 1
+        print("Getallen verlagen")
+    elif keuze == "g":
+        som = multiplication
+        reken = "x"
+        n2 = 1
+        print("Getallen verdubbelen")
+    elif keuze == "h":
+        som = division
+        reken = ":"
+        n2 = 2
+        print("Getallen halveren")
     print(f"{n1} {reken} {n2} = {som(number1=n1, number2=n2)}")
     print("--------------------------------------------------")
     print(f"Wat wilt u doen met de uitkomst ({som(number1=n1, number2=n2)})")
     n1 = som(number1=n1, number2=n2)
     keuze == True
     n2 = False
-    
-    
-    
-    
-    
-#Maak een functie voor de input 
-#Maak een functie voor de strings met return
-
-
-# als input int is krijg int terug
-#   als input str is krijg str terug
-
---
+++ module05_functions/test.py
@@ -0,0 +1,21 @@
+lijstje = {}
+
+while True:
+    boodschappen = input("Welke boodschappen heb je nodig? ")
+    if boodschappen == '':
+        input("Vul een product in: ")
+
+    hoeveelheid = int(input("Hoeveel heb je daarvan nodig? "))
+    if hoeveelheid == '':
+        int(input("Vul een getal in: "))
+    
+    lijstje.update({boodschappen: hoeveelheid})
+    
+    verder = input("Heb je meer spullen nodig? ")
+    if verder == 'nee':
+        break
+    elif verder != 'ja' and verder != 'nee':
+        input("Vul ja of nee in: ")
+
+
+print(lijstje)
