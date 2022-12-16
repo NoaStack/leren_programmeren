434e4f11062a7f9a901b3ef5824c1d6a8d0e72be
NoaStack <99070686@mydavinci.nl>
2022-12-16 14:03:34 +0100

    commit

--- module05_functions/fibonacci.py
+++ module05_functions/fibonacci.py
@@ -1,11 +1,12 @@
-
-def fibonaci(hoeveel):
-    nummers = 0,1
-    if hoeveel in nummers:
-        return hoeveel
-    return fibonaci(hoeveel - 1) + fibonaci(hoeveel - 2)
+def fibonacci(hoeveel):
+    n1, n2 = 0, 1
+    teller = 0
+    while teller < hoeveel:
+        print(n1)
+        nth = n1 + n2
+        n1 = n2
+        n2 = nth
+        teller +=1
     
-        
 
-for x in range(10):
-    print(fibonaci(x))
+fibonacci(hoeveel=10)
